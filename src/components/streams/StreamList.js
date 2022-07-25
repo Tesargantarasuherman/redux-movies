46cffeaf3148aa863cd536191a2339817bcd2d62
unknown <tgantara@gmail.com>
2022-07-25 14:39:43 +0700

    update

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions'
 
 export class StreamList extends Component {
   componentDidMount(){
     this.props.fetchStreams();
   }
+  renderAdmin(stream){
+    if(stream.userId === this.props.currentUserId){
+      return (
+        <div className="right floated content">
+            <button className="ui button primary">
+              Edit
+            </button>
+            <button className="ui button negative">
+              Delete
+            </button>
+        </div>
+      )
+    }
+  }
+  renderCreate(){
+    if(this.props.isSignedIn){
+      return(
+        <div style={{textAlign:'right'}}>
+          <Link to="/streams/new" className='ui button primary'>
+            Create Stream
+          </Link>
+        </div>
+      )
+    }
+  }
   renderList(){
     return  this.props.streams.map(stream=>{
       return(
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"/>
             <div className="content">
               {stream.title}
@@ -24,18 +51,21 @@ export class StreamList extends Component {
   render() {
     return (
       <>
-      <h2>Streams</h2>
-      <div className="ui celled list">
-        {this.renderList()}
-      </div>
+        <h2>Streams</h2>
+          {this.renderCreate()}
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
       </>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {streams:Object.values(state.streams)}
-}
+const mapStateToProps = (state) => ({
+  streams:Object.values(state.streams),
+  currentUserId:state.auth.userId,
+  isSignedIn:state.auth.isSignedIn
+})
 
 const mapDispatchToProps = {
   fetchStreams
