a721928afb67f5de542e80f62af9470495fc9569
unknown <tgantara@gmail.com>
2022-07-28 13:27:10 +0700

    update

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import history from '../../history';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const actions =(
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  )
+
+  return (
+  <div>
+    <Modal 
+      title="Delete Stream"
+      content="Are You Sure Want To Delete?"
+      actions={actions}
+      onDismiss={()=>history.goBack()}
+    />
+  </div>
+  );
 };
 
 export default StreamDelete;
